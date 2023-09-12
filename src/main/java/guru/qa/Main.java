c00ba30371f0bb4b34feb10f44a418a84fcc31b3
ivan <iv@jdf.com>
2023-09-12 19:35:13 +0300

    added.2testsOnGit

---
+++ src/main/java/guru/qa/Main.java
@@ -0,0 +1,8 @@
+package guru.qa;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+
+
+}
